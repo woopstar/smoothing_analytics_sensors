27eca3058703857f4a4adfc199735199500234f0
Andreas Krüger <andreas@kruger.nu>
2024-09-28 21:46:44 +0200

    Update config_flow.py

--- custom_components/ha_power_smoothing/config_flow.py
+++ custom_components/ha_power_smoothing/config_flow.py
@@ -1,6 +1,6 @@
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.const import CONF_NAME
+from homeassistant.core import callback
 from homeassistant.helpers import config_validation as cv
 from .const import DOMAIN
 
@@ -10,7 +10,7 @@ class PowerSmoothingConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input=None):
         errors = {}
         if user_input is not None:
-            return self.async_create_entry(title=user_input[CONF_NAME], data=user_input)
+            return self.async_create_entry(title="Power Sensor Smoothing", data=user_input)
 
         return self.async_show_form(
             step_id="user",
